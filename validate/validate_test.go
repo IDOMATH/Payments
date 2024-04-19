f61d7ef3afac1ee50c214175be5f5bbb95ea3493
IDOMATH <btdothemath@gmail.com>
2024-04-18 20:31:53 -0500

    Changing testing got/expected format.  Adding TestCardLength

--- validate/validate_test.go
+++ validate/validate_test.go
@@ -1,11 +1,23 @@
 package validate
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestLuhnCheckDigit(t *testing.T) {
 	amex := 371720518195098
+	expected := true
 	got := LuhnCheckDigit(amex)
-	if got != true {
-		t.Errorf("Expected true, got: %v", got)
+	if got != expected {
+		t.Errorf("Expected %v, got: %v", expected, got)
+	}
+}
+func TestCardLength(t *testing.T) {
+	card := 1234567890
+	expected := true
+	got := CardLength(card, len(strconv.Itoa(card)))
+	if got != expected {
+		t.Errorf("Expected %v, got: %v", expected, got)
 	}
 }
