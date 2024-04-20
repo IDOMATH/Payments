923ab96af622389ec5c4a84737579226fc4babe4
IDOMATH <btdothemath@gmail.com>
2024-04-19 22:24:31 -0500

    Adding test IIN exact

--- validate/validate_test.go
+++ validate/validate_test.go
@@ -5,8 +5,12 @@ import (
 	"testing"
 )
 
+const (
+	amex    = 371720518195098
+	amexIin = 37
+)
+
 func TestLuhnCheckDigit(t *testing.T) {
-	amex := 371720518195098
 	expected := true
 	got := LuhnCheckDigit(amex)
 	if got != expected {
@@ -17,6 +21,14 @@ func TestCardLength(t *testing.T) {
 	card := 1234567890
 	expected := true
 	got := CardLength(card, len(strconv.Itoa(card)))
+	if got != expected {
+		t.Errorf("Expected %v, got: %v", expected, got)
+	}
+}
+
+func TestIinExact(t *testing.T) {
+	expected := true
+	got := IinExact(amex, amexIin)
 	if got != expected {
 		t.Errorf("Expected %v, got: %v", expected, got)
 	}
